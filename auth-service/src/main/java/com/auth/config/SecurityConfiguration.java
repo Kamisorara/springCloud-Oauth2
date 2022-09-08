e9fa1b5d24fd77b10775eb60a44f0e66724f2146
kamisora <1210281722@qq.com>
2022-09-08 20:57:58 +0800

    添加Oauth2

--- auth-service/src/main/java/com/auth/config/SecurityConfiguration.java
+++ auth-service/src/main/java/com/auth/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -33,5 +34,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
     }
 }
--- book-service/src/main/java/com/book/controller/BookController.java
+++ book-service/src/main/java/com/book/controller/BookController.java
@@ -4,6 +4,8 @@ package com.book.controller;
 import com.book.service.BookService;
 import com.commons.entity.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,8 @@ public class BookController {
 
     @RequestMapping("/book/{bid}")
     Book findBookById(@PathVariable("bid") Long bid) {
+        SecurityContext context = SecurityContextHolder.getContext();
+        System.out.println(context.getAuthentication());
         return bookService.getBookById(bid);
     }
 }
